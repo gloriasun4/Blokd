26aca0ef472f1fd282e7939416035abcae740782
wsaid24 <wamiasaid02@gmail.com>
2021-04-24 17:59:22 -0400

    Updated files

--- popup.js
+++ popup.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded',function(){
     document.querySelector('button').addEventListener('click',onclick,false)
 
-    // function onclick(){
-    //     window.setTimeout(sendAfterTimeout,3000)
-    // }
+    function onclick(){
+        window.setTimeout(sendAfterTimeout,3000)
+        //sendAfterTimeout()
+    }
 
     function sendAfterTimeout(){
         chrome.tabs.query({currentWindow: true, active: true},function(tabs){
             chrome.tabs.sendMessage(tabs[0].id,'hi')
         })
+    }
+
+    function alertScreenTime(){
+        
+
     }
 })
