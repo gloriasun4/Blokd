80c6def3b1a15996fce2de412e20e3042de60926
wsaid24 <wamiasaid02@gmail.com>
2021-04-24 16:34:13 -0400

    Updated content.js

---
+++ content.js
@@ -0,0 +1,5 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+      const re = new RegExp('YouTube', 'gi')
+      const matches = document.documentElement.innerHTML.match(re)
+      sendResponse({count: matches.length})
+})
