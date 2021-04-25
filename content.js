39fa2cbdcca6e35bdf547db8dffe49c4cb529130
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-04-25 14:28:46 -0400

    Revert "Merge branch 'Extension' into main"
    This reverts commit 937c6eefe8bf107a9e12874e72b4ea34e75dcc97, reversing
    changes made to eb608b12036388cd42dd272e1790db5856021e6e.

--- content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    //   const re = new RegExp('YouTube', 'gi')
-    //   const matches = document.documentElement.innerHTML.match(re)
-      alert('request')
-      //sendResponse({count: matches.length})
-})
