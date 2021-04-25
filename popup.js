f22b50adac681e1557f4b669b980df146470d8ff
wsaid24 <wamiasaid02@gmail.com>
2021-04-24 22:29:47 -0400

    changed code to get button to work with alarm

--- popup.js
+++ popup.js
@@ -22,34 +22,34 @@ var alarmClock = {
     }
 };
 
-// document.addEventListener('DOMContentLoaded', function () {
-// alarmClock.setup();
-// });
+document.addEventListener('DOMContentLoaded', function () {
+alarmClock.setup();
+});
 
 
-// document.addEventListener('DOMContentLoaded',function(){
-//     document.querySelector('button').addEventListener('click',onclick,false)
+document.addEventListener('DOMContentLoaded',function(){
+    document.querySelector('button').addEventListener('click',onclick,false)
 
-//     function onclick(){
-//         chrome.alarms.create(
-//             "timer", {periodInMinutes: 1}
-//         )
+    function onclick(){
+        chrome.alarms.create(
+            "timer", {periodInMinutes: 1}
+        )
         
         
-//         //window.setTimeout(sendAfterTimeout,3000)
-//         //sendAfterTimeout()
-//         //alertScreenTime()
-//     }
+        //window.setTimeout(sendAfterTimeout,3000)
+        //sendAfterTimeout()
+        //alertScreenTime()
+    }
 
-//     function sendAfterTimeout(){
-//         chrome.tabs.query({currentWindow: true, active: true},function(tabs){
-//             chrome.tabs.sendMessage(tabs[0].id,'hi')
-//         })
-//     }
+    function sendAfterTimeout(){
+        chrome.tabs.query({currentWindow: true, active: true},function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id,'hi')
+        })
+    }
 
-//     function alertScreenTime(){
-//         //const screenTime = document.querySelector('h1')
-//         //screenTime.textContent = 'this is your screentime'
+    function alertScreenTime(){
+        //const screenTime = document.querySelector('h1')
+        //screenTime.textContent = 'this is your screentime'
 
-//     }
-// })
+    }
+})
