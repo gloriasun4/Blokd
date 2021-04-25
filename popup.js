0f2709d657e5f95f157e92c7a618e1509866ba3b
wsaid24 <wamiasaid02@gmail.com>
2021-04-25 14:15:29 -0400

    Revert "updates that need to revert"
    This reverts commit f7b7f72501a9c3a390a4a36b799ac3b42a2a4920.

--- popup.js
+++ popup.js
@@ -1,35 +1,41 @@
 // Set the date we're counting down to
-// var countDownDate = new Date();
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
 
-// countDownDate.setHours(countDownDate.getHours() + 1);
-// var countDownTime = countDownDate.getTime();
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-// // Update the count down every 1 second
-// var x = setInterval(function() {
+  // Get today's date and time
+  var now = new Date().getTime();
 
-//   // Get today's date and time
-//   var now = new Date().getTime();
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
 
-//   // Find the distance between now and the count down date
-//   var distance = countDownTime - now;
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-//   // Time calculations for days, hours, minutes and seconds
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
 
-//   // Display the result in the element with id="demo"
-//   document.getElementById("demo").innerHTML = hours + "h "
-//   + minutes + "m " + seconds + "s ";
+  //TODO If count down reaches 5 minutes, change color to yellow
+  if (distance < 5) {
+    //document.getElementById("demo").
+  }
+  //TODO If count down reaches 1 minutes, change color to red
+  if (distance < 1) {
 
-//   // If the count down is finished, write some text
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("demo").innerHTML = "Time's up!";
-//     getRandomImage();
-//   }
-// }, 1000);
+  }
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Time's up!";
+  }
+}, 1000);
+
+//TODO add random meme generation
 
 
 var alarmClock = {
